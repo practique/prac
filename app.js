7748e7f15e7057ee3665dd707ad8747e93f9426f
Cole Schiffer <coleschiffer@gmail.com>
2018-01-28 14:30:57 -0600

    layouts

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use(require('express-session')({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
